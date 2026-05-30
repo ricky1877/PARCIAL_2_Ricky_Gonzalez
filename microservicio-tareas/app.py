bc7ee0531a254cae70e75743e82c920727cc1a5b
ricky1877 <rick.gonzalez@duocuc.cl>
2026-05-30 05:33:28 +0000

    Agregar cobertura SAST y SCA

--- microservicio-tareas/app.py
+++ microservicio-tareas/app.py
@@ -19,4 +19,4 @@ def agregar_tarea():
     return jsonify(nueva_tarea), 201
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  # nosec B104
